24f6f414edee59cdfb8b6480b4009dd950b38422
Kevin Ushey <kevinushey@gmail.com>
2016-05-27 15:34:13 -0700

    screen clang in preproc checks

--- src/tests.h
+++ src/tests.h
@@ -4,7 +4,7 @@
 // Disable testthat when running with gcc 4.4
 // as it cannot handle some of the idioms used
 // by catch
-#if defined(__GNUC__) && defined(__GNUC_MINOR__)
+#if defined(__GNUC__) && defined(__GNUC_MINOR__) && !defined(__clang__)
 # if __GNUC__ < 4
 #  define TESTTHAT_DISABLED
 # elif __GNUC__ == 4 && __GNUC_MINOR__ < 6
