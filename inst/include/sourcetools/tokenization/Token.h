996e90e70d2ed55e24c5e887b23dd6b67c2c52d9
Kevin Ushey <kevinushey@gmail.com>
2016-09-14 15:15:11 -0700

    more use of 'std::'

--- inst/include/sourcetools/tokenization/Token.h
+++ inst/include/sourcetools/tokenization/Token.h
@@ -71,7 +71,7 @@ public:
 
   bool contentsEqual(const char* string)
   {
-    return ::strcmp(begin_, string);
+    return std::strcmp(begin_, string);
   }
 
   bool contentsEqual(const std::string& string) const
@@ -79,7 +79,7 @@ public:
     if (string.size() != size())
       return false;
 
-    return ::memcmp(begin_, string.c_str(), size()) == 0;
+    return std::memcmp(begin_, string.c_str(), size()) == 0;
   }
 
   const Position& position() const { return position_; }
@@ -369,8 +369,8 @@ inline bool parseUnicode(const char*& it, char*& output)
   }
 
   std::mbstate_t state;
-  ::memset(&state, 0, sizeof(state));
-  std::size_t bytes = ::wcrtomb(output, value, &state);
+  std::memset(&state, 0, sizeof(state));
+  std::size_t bytes = std::wcrtomb(output, value, &state);
   if (bytes == static_cast<std::size_t>(-1))
     return false;
 
