81f34812c4ccb5cff758c3f870beeaf0e60ea9b9
Kevin Ushey <kevinushey@gmail.com>
2016-10-17 20:29:32 -0700

    try simplifying Travis script

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -1,22 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 mkdir -p ~/.R
 
-if [ "${COMPILER}" = "gcc-4.4" ]; then
-    echo "CC=gcc-4.4 -std=gnu99"    >> ~/.R/Makevars
-    echo "CXX=g++-4.4"              >> ~/.R/Makevars
-    echo "CXX1X=g++-4.4 -std=c++0x" >> ~/.R/Makevars
-fi
+read -r -d '' MAKEVARS << EOF
+CC=${CC} -std=gnu99
+CXX=${CXX}
+CXX1X=${CXX} -std=c++0x
+EOF
 
-if [ "${COMPILER}" = "gcc" ]; then
-    echo "CC=gcc -std=gnu99"    >> ~/.R/Makevars
-    echo "CXX=g++"              >> ~/.R/Makevars
-    echo "CXX1X=g++ -std=c++0x" >> ~/.R/Makevars
-fi
-
-if [ "${COMPILER}" = "clang" ]; then
-    echo "CC=clang -std=gnu99"      >> ~/.R/Makevars
-    echo "CXX=clang++"              >> ~/.R/Makevars
-    echo "CXX1X=clang++ -std=c++0x" >> ~/.R/Makevars
-fi
-
+echo "${MAKEVARS}" > ~/.R/Makevars
