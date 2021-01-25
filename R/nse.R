31c46a4cd8f203435a4354eea464fd1d4ecc7c4d
Kevin Ushey <kevinushey@gmail.com>
2021-01-24 22:51:44 -0800

    un-export function

--- R/nse.R
+++ R/nse.R
@@ -1,7 +1,4 @@
-#' Detect Whether a Function Performs Non-Standard Evaluation
-#'
-#' @param ... \R functions.
-#' @export
+
 performs_nse <- function(...) {
   .Call(sourcetools_performs_nse, list(...))
 }
