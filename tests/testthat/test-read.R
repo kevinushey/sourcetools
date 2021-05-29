15263b7dcad552b007d1a1f2367c2482872faece
Kevin Ushey <kevinushey@gmail.com>
2021-05-28 22:04:13 -0700

    test fixups

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -8,69 +8,67 @@ files <- list.files(
 
 test_that("read_lines and readLines agree on output", {
   for (file in files) {
-    expect_identical(
-      readLines(file, warn = FALSE),
-      sourcetools::read_lines(file)
-    )
+    r <- readLines(file, warn = FALSE, encoding = "UTF-8")
+    s <- sourcetools::read_lines(file)
+    expect_identical(r, s)
   }
 })
 
 test_that("read and readChar agree on output", {
   for (file in files) {
-    expect_identical(
-      readChar(file, file.info(file)$size, TRUE),
-      sourcetools::read(file)
-    )
+    r <- readChar(file, file.info(file)$size, TRUE)
+    Encoding(r) <- "UTF-8"
+    s <- sourcetools::read(file)
+    expect_identical(r, s)
   }
 })
 
 test_that("read_bytes and readBin agree on output", {
   for (file in files) {
-    expect_identical(
-      readBin(file, "raw", file.info(file)$size),
-      sourcetools::read_bytes(file)
-    )
+    r <- readBin(file, "raw", file.info(file)$size)
+    s <- sourcetools::read_bytes(file)
+    expect_identical(r, s)
   }
 })
 
-test_that("read_lines can handle '\r' line endings", {
-  
+test_that("read_lines can handle '\\r' line endings", {
+
   file <- tempfile()
   on.exit(unlink(file), add = TRUE)
-  
+
   text <- "this\ris\rsome\rtext"
   writeLines(text, con = file, useBytes = TRUE)
-  
+
   r <- readLines(file)
   s <- read_lines(file)
   expect_identical(r, s)
-  
+
 })
 
-test_that("read_lines can handle '\r\n' line endings", {
-  
+test_that("read_lines can handle '\\r\\n' line endings", {
+
   file <- tempfile()
   on.exit(unlink(file), add = TRUE)
-  
+
   text <- "this\r\nis\r\nsome\r\ntext\r"
-  writeLines(text, con = file, useBytes = TRUE)
-  
+  writeBin(charToRaw(text), file)
+
   r <- readLines(file)
   s <- read_lines(file)
   expect_identical(r, s)
-  
+
 })
 
 test_that("read_lines can handle mixed line endings", {
-  
+
   file <- tempfile()
   on.exit(unlink(file), add = TRUE)
-  
+
   text <- "this\ris\nsome\r\ntext\r"
-  writeLines(text, con = file, useBytes = TRUE)
-  
+  writeBin(charToRaw(text), file)
+
   r <- readLines(file)
   s <- read_lines(file)
   expect_identical(r, s)
-  
+
 })
